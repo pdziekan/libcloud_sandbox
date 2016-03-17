dbd69383c9e4b23b3ae7d4fc4700e62f3c5dd89a
pdziekan <pdziekanp@gmail.com>
2016-03-17 13:47:44 +0100

    repo cleanup

--- compile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-g++ -std=c++11 libcloud_mpi_test.cpp -lcloudphxx_lgrngn
-
-
--- compile_mpi.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-mpic++ -std=c++11 libcloud_mpi_test.cpp -lcloudphxx_lgrngn
-
-#VERBOSE=1 LD_LIBRARY_PATH="/home/piotr/praca/libcloudphxx/build/src/" mpic++ -DSTD_FUTURE_WORKS -std=c++11  -fopenmp -pthread -g -I/home/piotr/praca/libcloudphxx/include profiling_test.cpp -lcloudphxx_lgrngn 
-
-
